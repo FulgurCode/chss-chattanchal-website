6051b5284029e50e5860eb7f0d06fecc99b2ddba
shreyas sk <shreyassk2005@gmail.com>
2023-04-14 11:40:36 +0530

    Formatting the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import './App.css'
-import { Link, Route, Routes } from 'react-router-dom'
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import Admin from "./pages/Admin"
-import AdminLogin from "./pages/admin/AdminLogin"
-import AdminAdmission from "./pages/admin/admission/AdminAdmission"
-import AdminNewAdmission from "./pages/admin/admission/AdminNewAdmission"
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Admin from "./pages/Admin";
+import AdminLogin from "./pages/admin/AdminLogin";
+import AdminAdmission from "./pages/admin/admission/AdminAdmission";
+import AdminNewAdmission from "./pages/admin/admission/AdminNewAdmission";
 
 export default function App() {
-  
   return (
     <>
       <Routes>
-
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/admin">
-
-            <Route index  element={<Admin/>}/>
-            <Route path="/admin/login" element={<AdminLogin/>}></Route>
-            <Route path="/admin/admission">
-
-              <Route index  element={<AdminAdmission/>}/>
-              <Route path="/admin/admission/new-admission" element={<AdminNewAdmission/>}></Route>
-
-            </Route>
-
+          <Route index element={<Admin />} />
+          <Route path="/admin/login" element={<AdminLogin />}></Route>
+          <Route path="/admin/admission">
+            <Route index element={<AdminAdmission />} />
+            <Route
+              path="/admin/admission/new-admission"
+              element={<AdminNewAdmission />}
+            ></Route>
+          </Route>
         </Route>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
