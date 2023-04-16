40d16df470e8a7d7fe9abeaea71cb5a9d11049e0
shreyas sk <shreyassk2005@gmail.com>
2023-04-16 18:24:08 +0530

    login page frontend

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,8 +1,44 @@
-export default function AdminLogin(){
+import schoolImg from "../imgs/school.png";
+import svgImg from "../imgs/footer.svg";
+import "./AdminLogin.css";
 
-    return (
-        <>
-            Login page for admin
-        </>
-    )
+export default function AdminLogin() {
+  return (
+    <>
+      <header>www.chsschattanchal.com</header>
+      <div className="main" style={{ backgroundImage: `url(${schoolImg})` }}>
+        <div className="container">
+          <div className="label">
+            <h2>Welocme to</h2>
+            <h1>
+              CHSS CHATTANCHAL<span></span>
+            </h1>
+          </div>
+          <div className="login">
+            <h1>
+              Login <span></span>
+            </h1>
+            <div className="loginElement">
+              <div>
+                <select placeholder="Select user type">
+                  <option value="student">Student</option>
+                  <option value="teacher">Teacher</option>
+                  <option value="admin">Admin</option>
+                </select>
+                <span></span>
+              </div>
+              <span>
+                <input placeholder="Admission Number" />
+                <input placeholder="Password" />
+                <a href="#">Forget password?</a>
+                <button>Login</button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer></footer>
+      <img src={svgImg} className="shapes" />
+    </>
+  );
 }
