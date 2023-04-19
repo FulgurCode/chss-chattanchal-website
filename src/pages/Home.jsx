d83b11b100e5b5786de6e9e4e2ee3844f9149017
Hrishikesh <2007hrishi@gmail.com>
2023-04-20 00:31:16 +0530

    Started working on the admin dashboard page. Created two h1 elements to access login page and admin-dash page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import { Outlet, Link } from "react-router-dom"
 export default function Home(){
 
     return (
         <>
-            <h1>Home page</h1>
+            <div>
+                <h1>Home page</h1>
+                <h1><Link to='/login'>login page</Link></h1>
+                <h1><Link to='/admin'>admin_dash</Link></h1>
+            </div>
+
+            <Outlet />
+
         </>
     )
 }
