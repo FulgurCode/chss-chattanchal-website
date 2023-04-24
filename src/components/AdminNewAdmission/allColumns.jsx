648c8ffe588b1f3ce45cdce800eea4ba58b6b162
KrishnajithK <krishnajithktuttu@gmail.com>
2023-04-24 20:54:22 +0530

    design complete for widescreen v1.0.0

--- src/components/AdminNewAdmission/allColumns.jsx
+++ src/components/AdminNewAdmission/allColumns.jsx
@@ -26,11 +26,108 @@ function AllColumns() {
                     <input className={`${styles.applicationNoInput} ${styles.inputField}`}></input>
                 </div>
                 <div className={`${styles.subContainer} ${styles.applicationNo}`}>
-                    <label className={`${styles.applicationNoLabel}`}>AdmissionLabel</label>
+                    <label className={`${styles.applicationNoLabel}`}>Admission Date</label>
                     <input className={`${styles.applicationNoInput} ${styles.inputField}`}></input>
                 </div>
             </div>
-            <hr className={`${styles.separationLine} ${styles.separationLineNormal}`} />
+            <hr className={`${styles.separationLine}`} />
+            <div className={`${styles.containerNew} `}>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.studentNameLabel} ${styles.label}`}>Name of the student <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.studentNameInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.adharNumLabel} ${styles.label}`}>Aadhar No <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.adharNumInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.genderLabel} ${styles.label}`}>Gender <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.genderInput} ${styles.inputFieldNew}`}></input>
+                </div>
+            </div>
+            <hr className={`${styles.separationLine}`} />
+            <div className={`${styles.containerNew} `}>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.parentNameLabel} ${styles.label}`}>Name of the parent / guardian <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.parentNameInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.parentOccupationLabel} ${styles.label}`}>Occupation of the parent / guardian <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.parentOccupationInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.relationshipLabel} ${styles.label}`}>Relationship of the student to guardian <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.relationshipInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.addressGuardianLabel} ${styles.label}`}>Address of guardian <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.addressGuardianInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.religionLabel} ${styles.label}`}>Religion <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.religionInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.casteLabel} ${styles.label}`}>Caste <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.casteInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.categoryLabel} ${styles.label}`}>Category <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.categoryInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.OBCLabel} ${styles.label}`}>Does the student belong to OBC <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.OBCInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.lanMinorityLabel} ${styles.label}`}>If the student belong to linguistic minority <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.lanMinorityInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.DOBLabel} ${styles.label}`}>DOB <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.DOBInput} ${styles.inputFieldNew}`}></input>
+                </div>
+            </div>
+            <hr className={`${styles.separationLine}`} />
+            <div className={`${styles.containerNew}`}>
+                <label className={`${styles.subHeadingLabel}`}>Details of qualifying examination</label>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.boardNameLabel} ${styles.label}`}>Name of Board <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.boardNameInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.regNoLabel} ${styles.label}`}>Registration No. <span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.regNoInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.monthAndYearOfPassingLabel} ${styles.label}`}>Month and year of passing<span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.monthAndYearOfPassingInput} ${styles.inputFieldNew}`}></input>
+                </div>
+            </div>
+            <hr className={`${styles.separationLine}`} />
+            <div className={`${styles.containerNew}`}>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.classAndCombinationLabel} ${styles.label}`}>Class and combination in which admitted<span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.classAndCombinationInput} ${styles.inputFieldNew}`}></input>
+                </div>
+            </div>
+            <hr className={`${styles.separationLine}`} />
+            <div className={`${styles.containerNew}`}>
+                <label className={`${styles.subHeadingLabel}`}>Details of Transfer certificate produced on admission</label>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.TCNumberLabel} ${styles.label}`}>Number<span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.TCNumberInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.TCdateLabel} ${styles.label}`}>Date<span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.TCdateInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <div className={`${styles.subContainerNew}`}>
+                    <label className={`${styles.issuedSchoolLabel} ${styles.label}`}>Issued school / institution<span className={`${styles.aster}`} >  *  </span></label>
+                    <input className={`${styles.issuedSchoolInput} ${styles.inputFieldNew}`}></input>
+                </div>
+                <button className={`${styles.submitButton}`} >Submit</button>
+            </div>
         </div>
     )
 }
--- src/pages/admin/admission/AdminNewAdmission.jsx
+++ src/pages/admin/admission/AdminNewAdmission.jsx
@@ -1,13 +1,12 @@
 import AllColumns from "../../../components/AdminNewAdmission/allColumns";
 import NavBar from "../../../components/NavBar";
 
-
 export default function AdminNewAdmission() {
 
     return (
-        <>
+        <div style={{ overflowY: 'auto', maxHeight: '100vh' }}>
             <NavBar />
             <AllColumns />
-        </>
+        </div>
     )
 }
