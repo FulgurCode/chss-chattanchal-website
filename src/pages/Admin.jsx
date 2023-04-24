6295531aa78d1bbcac8658db15dd7696a5191ef9
Hrishikesh <2007hrishi@gmail.com>
2023-04-24 16:44:27 +0530

    Navbar fixed

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,25 +1,17 @@
-import styles from '../../styles/admin/admin.module.css'
+import styles from '../styles/admin/admin.module.css'
 import { Link, Outlet } from 'react-router-dom'
-
+import NavBar from '../components/NavBar'
 import React from 'react'
-import menu_img from '/imgs/menu.png'
+// import menu_img from '/imgs/menu.png'
 import admission from '/imgs/admission.png'
-import user from '/imgs/user.png'
+// import user from '/imgs/user.png'
 import attendence from '/imgs/attendence.png'
 
 export default function Admin(){
     return (
         <>  
             
-            <header className={styles.adminHeader}>
-                <Link to='/admin'><div className={styles.adminHomebutton}>
-                <img src={menu_img} className={styles.headerHomebuttonImg} />
-                    <h3  className={styles.headerHomebuttonText}>Home</h3>
-                </div></Link>
-                <h3 className={styles.headerTitle}>CHSS CHATTANCHAL</h3>
-                <img src={user} className={styles.userimg}>
-                </img>
-            </header>
+            <NavBar />
             <section>
                 <div className={styles.admission}>
                     <Link to="/admin/admission"><img className={styles.iconadmission} src={admission} /></Link>
