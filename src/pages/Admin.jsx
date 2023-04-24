b9ecd531c8d49b84c2dedf0a9bfafbeb55aa372b
Hrishikesh-MS <119434739+Hrishikesh-MS@users.noreply.github.com>
2023-04-24 17:10:56 +0530

    Admin page added

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,3 +1,24 @@
-export default function Admin() {
-  return <>Admin page</>;
+import styles from '../styles/admin/admin.module.css'
+import { Link, Outlet } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+import React from 'react'
+// import menu_img from '/imgs/menu.png'
+import admission from '/imgs/admission.png'
+// import user from '/imgs/user.png'
+import attendence from '/imgs/attendence.png'
+
+export default function Admin(){
+    return (
+        <>  
+            
+            <NavBar />
+            <section>
+                <div className={styles.admission}>
+                    <Link to="/admin/admission"><img className={styles.iconadmission} src={admission} /></Link>
+                    <img className={styles.iconattendence} src={attendence} />
+                </div>
+            </section>
+
+        </>
+    )
 }
