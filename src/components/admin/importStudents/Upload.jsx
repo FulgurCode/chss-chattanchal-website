ec95febbb8ee5d36a42aaab0e7d660e1ca8e2608
Hrishikesh <2007hrishi@gmail.com>
2023-05-02 21:18:27 +0530

    ImportStudents page is ready
    In process of Import CSV
    
    In process of Import CSV two
    
    ImportStudents page is completed
    
    ImportStudents page is ready

---
+++ src/components/admin/importStudents/Upload.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "../../../styles/admin/admission/importStudents/Importstudents.module.css";
+import Axios from "../../../../stores/Axios";
+
+function Upload() {
+  // for error
+  const [msg, setMsg] = useState("");
+  const [instText, setInstText] = useState("Click to change file")
+
+  //
+  const [file, setFile] = useState(null);
+
+  function handleFileUpload(event){
+    setFile(event.target.files[0]);
+  };
+
+  function handleFileSubmit(event){
+
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    Axios.post("http://localhost:9000/api/admin/import-students", formData)
+      .then((response) => {
+        console.log(response.data);
+        setMsg(response.data);
+        changeMsgColor("Green");
+        setInstText("New File")
+      })
+      .catch((error) => {
+        changeMsgColor("red")
+        if (error.response.status == 401) {
+          setMsg(error.response.data);
+        } else {
+          setMsg(error.response.data);
+        }
+      });
+
+  }
+  
+  function changeMsgColor(color){
+    const header = document.getElementById("message")
+    header.style.color = color
+
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.hr}></div>
+
+        <span className={styles.path}>
+          Home &gt; Admin &gt; Admission &gt;
+          <span>Import Students</span>
+        </span>
+
+        <div className={styles.uploadContainer}>
+          <div className={styles.uploadBox}>
+            <div className={styles.info}>
+              <label htmlFor="csv">
+                {file ? <p>{instText}</p> : <p>Choose file</p>}
+              </label>
+              {file && <p>{file.name}</p>}
+
+              <input
+                id="csv"
+                type="file"
+                accept=".csv"
+                onChange={handleFileUpload}
+              />
+            </div>
+            <div className={styles.msg}>
+              {msg ? (
+                <h5 id="message">{msg}</h5>
+              ) : (
+                <h5 id="message">Upload CSV File Above</h5>
+              )}
+            </div>
+            <button type="submit" onClick={handleFileSubmit}>
+              Submit
+            </button>
+          </div>
+        </div>
+        <div className={styles.under}> </div>
+        <div className={styles.note}>
+          <p>
+            <b>Note:</b>
+            <br />
+            It is mandatory to upload student details only in CSV file format.
+            No other formats are acceptable.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Upload;
--- src/pages/admin/admission/ImportStudents.jsx
+++ src/pages/admin/admission/ImportStudents.jsx
@@ -1,3 +1,21 @@
+import React from "react";
+import NavBar from "../../../components/NavBar";
+import styles from "../../../styles/admin/admission/importStudents/Importstudents.module.css";
+import Upload from "../../../components/admin/importStudents/Upload";
+
 export default function ImportStudents() {
-  return <>Import studets in admin </>;
+  return (
+    <>
+      <NavBar />
+      <div className={styles.hero}>
+        <img src="/imgs/importStudents/download.png" className={styles.downIcon} />
+        <h2 className={styles.title}>Import Students</h2>
+        <div className={styles.side}></div>
+      </div>
+      <Upload /> 
+      
+    </>
+    
+    
+    )
 }
