8599ffd36b18e8480836b89ee58532cef93ab180
Hrishikesh M S <119434739+Hrishikesh-MS@users.noreply.github.com>
2023-05-04 02:35:08 +0530

    Navbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,27 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
-import styles from "../styles/NavBar.module.css"
-import userImg from "/imgs/User-40.svg"
-import msgImg from "/imgs/msg_icon.svg"
-import menuButton from "/imgs/menu_button.svg"
-import bellIcon from "/imgs/bell_icon.svg"
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function NavBar() {
-    const navigate = useNavigate()
-    return (
-        <div className={styles.navBar}>
-            <img className={`${styles.navImg} ${styles.userImg}`} src={userImg} />
-            <label className={styles.schlName}>CHSS CHATTANCHAL</label>
-            <div className={styles.vl1} />
-            <img className={`${styles.navImg} ${styles.msgImg}`} src={msgImg} />
-            <img className={`${styles.navImg} ${styles.menuButton}`} src={menuButton} onClick={() => {
-                navigate("/admin");
-              }}/>
-            <img className={`${styles.navImg} ${styles.bellIcon}`} src={bellIcon} />
-        </div>
-    )
+import styles from "../styles/NavBar.module.css";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.home} onClick={() => navigate("/admin")}>
+        <img className={styles.homeImg} src="/imgs/menu_button.svg" />
+        <label className={styles.homeText}>Home</label>
+      </div>
+
+      <div className={styles.title}>
+        <h4 className={styles.school}>CHATTANCHAL HSS</h4>
+      </div>
+
+      <div className={styles.icons}>
+        <img className={styles.iconImg} src="/imgs/msg_icon.svg" />
+        <img className={styles.iconImg} src="/imgs/bell_icon.svg" />
+        <img className={styles.iconImg} src="/imgs/user.png" />
+      </div>
+    </header>
+  );
 }
-
-export default NavBar
