28f2cba50282d735cb6b67ae79582862296563fe
krishnajithrgv <krishnajithktuttu@gmail.com>
2023-05-08 00:23:43 -0700

    web cam bug fix

--- src/components/admin/newAdmission/WebCamPopUp.jsx
+++ src/components/admin/newAdmission/WebCamPopUp.jsx
@@ -1,75 +1,62 @@
 import React, { useEffect, useRef } from "react";
-import popUpStyles from "../../../styles/admin/admission/newAdmission/PopUp.module.css";
+import popUpStyles from "../../../styles/admin/admission/newAdmission/Popup.module.css";
 import Webcam from "react-webcam";
 
 export default function WebCamPop(props) {
-
-
-  let videoRef = useRef(null);
-  let photoRef = useRef(null);
-
-  // get access to user camera 
-
-  const getUserCamera = () => {
-    navigator.mediaDevices.getUserMedia({
-      video:true
-    })
-    .then(
-      (stream) => {
-        let video = videoRef.current
-        video.srcObject = stream
-        video.play()
-      }
-      
-    )
-    .catch((err) => {
-      console.error(err)
-    })
-  }
+  const webcamRef = useRef(null);
+  const photoRef = useRef(null);
 
   useEffect(() => {
-    getUserCamera()
-  }, [videoRef])
+    const getUserCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const video = webcamRef.current.video;
+        video.srcObject = stream;
+        video.play();
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-  if (props.open == false) {
+    getUserCamera();
+  }, []);
+
+  const takePhoto = (e) => {
+
+    e.preventDefault()
+
+    const width = 400;
+    const height = 300;
+
+    const photo = photoRef.current;
+    const video = webcamRef.current.video;
+
+    photo.width = width;
+    photo.height = height;
+
+    const context = photo.getContext("2d");
+    context.drawImage(video, 0, 0, photo.width, photo.height);
+  };
+
+  if (props.open === false) {
     return null;
   }
 
-  // take photo
-
-  const takePhoto = () => {
-
-    let width = 400
-
-    let height = 300
-
-    let photo = photoRef.current
-
-    let video = videoRef.current
-
-    photo.width = width
-    photo.height = height 
-
-    let context = photo.getContext("2d")
-    context.drawImage(video, 0, 0, photo.width, photo.height)
-  }
-
   return (
-    <div onClick={() => props.show(!props.open)} className={popUpStyles.overlay}>
-      <div className={`${popUpStyles.popupBodyWebCam}`}>
-        <video ref={videoRef} className={popUpStyles.canvas}></video>
+    <div className={popUpStyles.overlay}>
+      <div className={popUpStyles.popupBodyWebCam}>
+        <Webcam ref={webcamRef} className={popUpStyles.canvas} />
         <button
           onClick={() => {
             props.show(!props.open);
           }}
-          className={`${popUpStyles.close}`}
+          className={popUpStyles.close}
         >
           X
         </button>
-        <button onClick={takePhoto} className={popUpStyles.clickButton}></button>
-        <canvas ref={photoRef} ></canvas>
+        <button onClick={takePhoto} className={popUpStyles.clickButton} />
+        <canvas ref={photoRef} />
       </div>
-
     </div>
   );
 }
