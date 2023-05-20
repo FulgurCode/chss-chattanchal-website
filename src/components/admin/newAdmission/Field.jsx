665e1341555c67b4e59888f2fb06568d0644cbd6
krishnajithktuttu <98537217+krishnajithktuttu@users.noreply.github.com>
2023-05-20 13:57:35 +0530

    new-admission page completed

---
+++ src/components/admin/newAdmission/Field.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "../../../styles/admin/admission/newAdmission/AllColumns.module.css"
+
+export default function Field(props) {
+  return (
+    <div className={props.containerClass}>
+      <label className={`${styles.label}`}>
+        {props.text} <span className={`${styles.aster}`}> * </span>
+      </label>
+      <input
+        type={props.type}
+        accept={props.extention}
+        min={props.min}
+        max={props.max}
+        onChange={props.change}
+        value={props.value}
+        name={props.name}
+        className={`${props.inputStyle} ${styles.inputFieldNew}`}
+        ref={props.reference}
+      ></input>
+    </div>
+  );
+}
---
+++ src/components/admin/newAdmission/WebCamPopUp.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef } from "react";
+import popUpStyles from "../../../styles/admin/admission/newAdmission/Popup.module.css";
+import Webcam from "react-webcam";
+
+export default function WebCamPop(props) {
+  const webcamRef = useRef(null);
+
+  
+  const videoConstraints = {
+    aspectRatio: 1 / 1, // Set the desired aspect ratio
+  };
+
+  useEffect(() => {
+    const getUserCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        const video = webcamRef.current.video;
+        video.srcObject = stream;
+        video.play();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserCamera();
+  }, []);
+
+  
+  async function base64ToFile(dataUrl, setState) {
+    let blob = await fetch(dataUrl).then((res) => res.blob());
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      const arrayBuffer = fileReader.result;
+      const file = new File([arrayBuffer], "file.jpg", {
+        type: blob.type,
+      });
+      setState(file);
+    };
+    fileReader.readAsArrayBuffer(blob);
+  }
+
+  const takePhoto = async (e) => {
+    e.preventDefault();
+
+    const width = 400;
+    const height = 300;
+
+    const photo = props.photoRef.current;
+    const video = webcamRef.current.video;
+    props.setGlobal(false);
+    if (photo && video) {
+      photo.width = width;
+      photo.height = height;
+
+      const context = photo.getContext("2d");
+      context.drawImage(video, 0, 0, photo.width, photo.height);
+
+      // Convert canvas to data URL
+      const dataUrl =  photo.toDataURL("image/jpg");
+      const photoFile = await base64ToFile(dataUrl, props.webCamPhoto) 
+      // props.webCamPhoto(photoFile)
+      // props.webCamPhoto(dataUrl)
+      props.inputRef.current.value = ""
+      props.show(false);
+      props.setGlobal(false)
+    }
+  };
+
+  if (props.open === false) {
+    return null;
+  }
+
+  return (
+    <div className={popUpStyles.overlay}>
+      <div className={popUpStyles.popupBodyWebCam}>
+        <Webcam ref={webcamRef} className={popUpStyles.canvas} videoConstraints={videoConstraints} />
+        <button
+          onClick={() => {
+            props.show(!props.open);
+          }}
+          className={popUpStyles.close}
+        >
+          X
+        </button>
+        <button onClick={takePhoto} className={popUpStyles.clickButton} />
+      </div>
+    </div>
+  );
+}
